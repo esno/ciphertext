8d020438fc9599f7d0fd90ad46a7f16a587ef0f9
crito <crito@fnordpipe.org>
2017-06-20 03:40:16 +0200

    add decrypt method

--- src/js/ciphertext.js
+++ src/js/ciphertext.js
@@ -1,25 +1,76 @@
+function _decrypt(key, data) {
+  var a = data
+    .replace('-----BEGIN CIPHERTEXT-----', '')
+    .replace('------END CIPHERTEXT------', '')
+    .trim();
+  var b = atob(a);
+
+  var buf = new Uint8Array(b.length);
+  Array.prototype.forEach.call(b, function(byte, i) {
+    buf[i] = byte.charCodeAt(0);
+  });
+
+  var iv = new Uint8Array(12);
+  var enc = new Uint8Array(buf.length - 12);
+  Array.prototype.forEach.call(buf, function(byte, i) {
+    if (i < 12) {
+      iv[i] = byte;
+    } else {
+      enc[i - 12] = byte;
+    }
+  });
+
+  window.crypto.subtle.decrypt(
+    {
+      name: "AES-GCM",
+      iv: iv,
+      tagLength: 128
+    },
+    key,
+    enc
+  )
+  .then(function(e) {
+    return _exitLog(new TextDecoder('utf-8').decode(e));
+  })
+  .catch(function(e) {
+    return _exitLog('wrong token!');
+  });
+}
+
 function _encrypt(key, data) {
+  var iv = window.crypto.getRandomValues(new Uint8Array(12));
+
   window.crypto.subtle.encrypt(
     {
       name: 'AES-GCM',
-      iv: window.crypto.getRandomValues(new Uint8Array(12)),
+      iv: iv,
       tagLength: 128
     },
     key,
     new TextEncoder('utf-8').encode(data)
   )
   .then(function(e) {
-    var b = btoa(
+    var enc = new Uint8Array(e);
+    var buf = new Uint8Array(iv.length + enc.length);
+    Array.prototype.forEach.call(iv, function(byte, i) {
+      buf[i] = byte;
+    });
+    Array.prototype.forEach.call(enc, function(byte, i) {
+      buf[12 + i] = byte;
+    });
+
+    var a = btoa(
       String.fromCharCode.apply(
         null,
-        new Uint8Array(e)
+        buf
       )
     );
-    h = '-----BEGIN CIPHERTEXT-----';
-    f = '------END CIPHERTEXT------';
-    b = h + "\n" + b + "\n" + f;
 
-    return _exitLog(b);
+    var h = '-----BEGIN CIPHERTEXT-----';
+    var f = '------END CIPHERTEXT------';
+    var ct = h + "\n" + a + "\n" + f;
+
+    return _exitLog(ct);
   })
   .catch(function(e) {
     return _exitLog(e);
@@ -32,9 +83,17 @@ function _exitLog(msg) {
   return false;
 }
 
+function _isEncrypted(input) {
+  if(input.startsWith('-----BEGIN CIPHERTEXT-----') && input.endsWith('------END CIPHERTEXT------')) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
 function compute() {
   var token = document.getElementById('token').value;
-  var input = document.getElementById('input').value;
+  var input = document.getElementById('input').value.trim();
   var ret = false;
 
   if(token.length == 0) {
@@ -58,7 +117,11 @@ function compute() {
       [ 'encrypt', 'decrypt' ]
     )
     .then(function(k) {
-      ret = _encrypt(k, input);
+      if(_isEncrypted(input)) {
+        ret = _decrypt(k, input);
+      } else {
+        ret = _encrypt(k, input);
+      }
     })
     .catch(function(e) {
       ret =  _exitLog(e);
